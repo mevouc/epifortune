3d7c5b0c61a4e2d04a28a72074b90e1946cf1f1d
mevouc <meven.courouble@vouc.me>
2015-12-20 15:41:39 +0000

    Implementation of --help display

--- src/epifortune.c
+++ src/epifortune.c
@@ -23,3 +23,28 @@ int main(int argc, char **argv)
   system("rm -f [0-9]*");
   return 0;
 }
+
+void print_help(void)
+{
+  printf("epifortune: Syntax: epifortune [-h] [-n number] [-c acqn]\n");
+  printf("\n");
+  printf("  -h | --help           Prompt this help.\n");
+  printf("  -n | --number number  Print the number-th quote. If this option ");
+  printf("is not set,\n");
+  printf("                        a random quote will be printed.\n");
+  printf("  -c | --conceal acqn   Hide one or more infos about the quotation:");
+  printf("                          * a - Author\n");
+  printf("                          * c - Context\n");
+  printf("                          * q - Quote\n");
+  printf("                          * n - Number\n");
+  printf("\n");
+  printf("Examples:\n");
+  printf("  epifortune\n");
+  printf("  epifortune --help\n");
+  printf("  epifortune -n 42 -c n\n");
+  printf("  epifortune -c aqn\n");
+  printf("\n");
+  printf("Provided by Meven 'mevouc' Courouble (http://vouc.me).\n");
+  printf("Code (https://github.com/mevouc/epifortune) under Beerware ");
+  printf("licence.\n");
+}
--- src/epifortune.h
+++ src/epifortune.h
@@ -1,6 +1,6 @@
 # ifndef _EPIFORTUNE_H_
 # define _EPIFORTUNE_H_
 
-
+void print_help(void);
 
 # endif
